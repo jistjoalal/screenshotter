c3cd3f9fe20959e3e20f09e0c47e050c0e93ca64
jistjoalal <jistjoalal@gmail.com>
2019-06-13 03:09:15 -0700

    try heroku deploy again...

--- screenshotter.js
+++ screenshotter.js
@@ -7,7 +7,9 @@ const screenshot = async (url, mobile = false) => {
   const path = makePath(url, mobile);
 
   // setup page
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
   const page = await browser.newPage();
 
   try {
