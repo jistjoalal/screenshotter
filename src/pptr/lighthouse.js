30876ab4b566ba4b94efff0c5ae86c64f836b545
jistjoalal <jistjoalal@gmail.com>
2019-06-18 05:01:34 -0700

    tshoot lighthouse report in staging env - apparently dont need to run in headful :P

--- src/pptr/lighthouse.js
+++ src/pptr/lighthouse.js
@@ -1,19 +1,15 @@
 const { URL } = require("url");
 const lighthouse = require("lighthouse");
-const puppeteer = require("puppeteer");
+
+const { initBrowser } = require("./browser");
 
 const lighthouseReport = async url => {
   try {
-    const browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      headless: false,
-      defaultViewport: null
-    });
+    const browser = await initBrowser();
     const result = await lighthouse(url, {
       port: new URL(browser.wsEndpoint()).port,
       output: "html"
     });
-    await browser.close();
     return result.report;
   } catch (err) {
     throw err;
